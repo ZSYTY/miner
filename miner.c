29213068a2291918dfcb5bd5b3adad5ce89b3387
ZSYTY <793533676@qq.com>
2019-05-17 17:23:11 +0800

    feat: import libgraphics

---
+++ miner.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include "graphics.h"
+#include "genlib.h"
+#include "conio.h"
+
+#include <windows.h>
+#include <olectl.h>
+#include <stdio.h>
+#include <mmsystem.h>
+#include <wingdi.h>
+#include <ole2.h>
+#include <ocidl.h>
+#include <winuser.h>
+
+void Main() {
+	InitGraphics();
+}
